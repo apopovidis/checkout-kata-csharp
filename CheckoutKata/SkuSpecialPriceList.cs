1aab97b88b6eb1baea0c14365dc8ecb62aeac030
Antonis Popovidis <antonispopovidis@gmail.com>
2021-06-24 22:10:21 +0300

    - Add trim space to name in skuspecialpricelist AddItem method - Overload GetSkus to accept a variable number of params to support lookup scenarios - Modify GetSkus to disallow skus with same number of units and price

--- CheckoutKata/SkuSpecialPriceList.cs
+++ CheckoutKata/SkuSpecialPriceList.cs
@@ -11,9 +11,10 @@ namespace CheckoutKata
         public void AddItem(string name, int numberOfUnits, int totalPrice)
         {
             char[] charsToTrim = { ' ' };
-            if (name.Trim(charsToTrim) == "")
+            string n = name.Trim(charsToTrim);
+            if (n == "")
             {
-                throw new ArgumentException(String.Format("{0} cannot be empty", name), "name");
+                throw new ArgumentException(String.Format("{0} cannot be empty", n), "name");
             }
 
             if (numberOfUnits <= 0)
@@ -26,25 +27,29 @@ namespace CheckoutKata
                 throw new ArgumentException(String.Format("{0} must be greater than zero", totalPrice), "totalPrice");
             }
 
-            var skus = this.GetSkus(name, numberOfUnits);
+            var skus = this.GetSkus(n, numberOfUnits, totalPrice);
             if (skus.Count == 0)
             {
-                this.items.Add(new SkuSpecialPrice(name, numberOfUnits, totalPrice));
+                this.items.Add(new SkuSpecialPrice(n, numberOfUnits, totalPrice));
             }
             else {
-                throw new Exception(String.Format("sku with name {0} and numberOfUnits {1} already exists", name, numberOfUnits));
+                throw new Exception(String.Format("sku with name {0}, numberOfUnits {1} and total price {2} already exists", name, numberOfUnits, totalPrice));
             }
         }
 
-        public List<SkuSpecialPrice> GetSkus(string name, int numberOfUnits = 0)
+        public List<SkuSpecialPrice> GetSkus(string name, int numberOfUnits, int totalPrice)
         {
-            if (numberOfUnits > 0)
-            {
-                return this.items.Where(i => i.GetName() == name && i.GetNumberOfUnits() == numberOfUnits).ToList();
-            }
-            else {
-                return this.items.Where(i => i.GetName() == name).ToList();
-            }
+            return this.items.Where(i => i.GetName() == name && i.GetNumberOfUnits() == numberOfUnits && i.GetTotalPrice() == totalPrice).ToList();
+        }
+
+        public List<SkuSpecialPrice> GetSkus(string name, int numberOfUnits)
+        {
+            return this.items.Where(i => i.GetName() == name && i.GetNumberOfUnits() == numberOfUnits).ToList();
+        }
+
+        public List<SkuSpecialPrice> GetSkus(string name)
+        {
+            return this.items.Where(i => i.GetName() == name).ToList();
         }
 
         public int GetSkuSpecialPrice(string name, int numberOfUnits)
